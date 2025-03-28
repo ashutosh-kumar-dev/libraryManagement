b5c28fbabdad6bd37b397e147cd9f48973c8e66d
ashutosh-kumar-dev <91617603+ashutosh-kumar-dev@users.noreply.github.com>
2025-03-28 10:58:53 +0530

    Update BookRepository.java

--- backend/src/main/java/com/library/repository/BookRepository.java
+++ backend/src/main/java/com/library/repository/BookRepository.java
@@ -3,7 +3,9 @@ package com.library.repository;
 
 import com.library.entity.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,6 +14,9 @@ public interface BookRepository extends JpaRepository<BookEntity, Long> {
 
     BookEntity findByBookId(Long bookId);
     List<BookEntity> findByTitle(String title);
+
+    @Modifying
+    @Transactional
     void deleteByBookId(Long bookId);
 
 }
